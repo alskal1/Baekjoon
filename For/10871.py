80889bd78ca5d35a485144ad4ba5954da21e5d5a
alskal1 <uiyn@naver.com>
2020-12-29 00:24:42 +0900

    백준 단계별 풀어보기(for문)

--- For/10871.py
+++ For/10871.py
@@ -1,6 +1,6 @@
-import sys
-
 n, x = map(int, input().split())
+a = list(map(int, input().split()))
+
 for i in range(n):
-    a = map(int, input().split())
-    print(a)
+    if a[i]<x:
+        print(a[i], end=" ")
